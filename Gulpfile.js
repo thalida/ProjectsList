67f0ed1f07a5e491c011f963c5e72ff2a1751502
Thalida Noel <thalida.c.noel@gmail.com>
2015-11-26 18:17:51 -0500

    Setup rsync

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var connect = require('gulp-connect');
 var browserify = require('browserify');
+var rsync = require('gulp-rsync');
 var source = require('vinyl-source-stream');
+var config = require('./buildConfig');
 
 gulp.task('connect', function(){
 	connect.server({
@@ -21,7 +23,7 @@ gulp.task('browserify', function( type ){
 
 gulp.task('browserify-dist', function( type ){
 	console.log( type )
-	return browserify({ entries: ['./app/app.js', './app/analytics.js'] })
+	return browserify({ entries: ['./app/app.js'] })
 		.bundle()
 		.pipe(source('app.js'))
 		.pipe(gulp.dest('public/js/'));
@@ -45,4 +47,7 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default', ['browserify', 'sass', 'images', 'connect', 'watch']);
-gulp.task('deploy', ['browserify-dist', 'sass', 'images']);
+gulp.task('deploy', ['browserify-dist', 'sass', 'images'], function(){
+	var rsyncConfig = config.rsync;
+	return gulp.src(rsyncConfig.src).pipe(rsync(rsyncConfig.options))
+});
